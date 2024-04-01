d247c4ad4c631c7044db5f3c89b1f1b179593f62
tylerwarwick <tylerwarwickwork@gmail.com>
2024-03-31 22:18:08 -0400

    Syntax errors

--- Control1.vhd
+++ Control1.vhd
@@ -132,7 +132,7 @@ begin
 				reg_src		<=	"10";
 				mem_read		<=	'0';
 				mem_write	<=	'0';
-				pc_src      <=  "01" when zero='0' else '00';
+				pc_src      <=  "01" when zero='0' else "00";
 
 			-- op=B, JUMP
 			when x"B" =>
